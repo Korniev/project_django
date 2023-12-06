bea672fac6ec9cddb20c9602520619f286594ea5
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-06 18:31:40 +0100

    new commit

--- app_instagram/app_photo/urls.py
+++ app_instagram/app_photo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('free-pictures/', views.pictures, name='pictures'),
     path('upload/', views.upload, name='upload'),
+    path('free-pictures/edit/<int:pic_id>', views.upload, name='edit'),
+    path('free-pictures/remove/<int:pic_id>', views.upload, name='remove'),
 ]
--- app_instagram/app_photo/views.py
+++ app_instagram/app_photo/views.py
@@ -29,3 +29,29 @@ def upload(request):
             pic.save()
             return redirect(to='app_photo:pictures')
     return render(request, 'app_photo/upload.html', context={'form': form})
+
+
+@login_required
+def edit(request, pic_id):
+    form = PictureForm(instance=Picture())
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES, instance=Picture())
+        if form.is_valid():
+            pic = form.save(commit=False)
+            pic.user = request.user
+            pic.save()
+            return redirect(to='app_photo:pictures')
+    return render(request, 'app_photo/upload.html', context={'form': form})
+
+
+@login_required
+def remove(request, pic_id):
+    form = PictureForm(instance=Picture())
+    if request.method == 'POST':
+        form = PictureForm(request.POST, request.FILES, instance=Picture())
+        if form.is_valid():
+            pic = form.save(commit=False)
+            pic.user = request.user
+            pic.save()
+            return redirect(to='app_photo:pictures')
+    return render(request, 'app_photo/upload.html', context={'form': form})
