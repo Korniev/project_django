2510dc85d69be016e0424f78b4e94d7c3b904228
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-06 17:32:54 +0100

    update

--- app_instagram/app_auth/views.py
+++ app_instagram/app_auth/views.py
@@ -10,6 +10,11 @@ class RegisterView(View):
     template_name = 'app_auth/register.html'
     form_class = RegisterForm
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(to='app_photo:home')
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         return render(request, self.template_name, {'form': self.form_class})
 
--- app_instagram/app_photo/models.py
+++ app_instagram/app_photo/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -9,6 +10,8 @@ def validate_file_size(value):
         raise ValidationError("Max file size - 1 mb")
     return value
 
+
 class Picture(models.Model):
     description = models.CharField(max_length=300)
     path = models.ImageField(upload_to='images', validators=[])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, default=None)
