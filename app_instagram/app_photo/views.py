e75d2811510d8a20244d169180dd8247e1e8a6f0
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2023-12-06 20:54:49 +0100

    updated

--- app_instagram/app_photo/views.py
+++ app_instagram/app_photo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from pathlib import Path
+from django.conf import settings
 from .forms import PictureForm
 from .models import Picture
 from django.contrib.auth.decorators import login_required
@@ -33,25 +34,23 @@ def upload(request):
 
 @login_required
 def edit(request, pic_id):
-    form = PictureForm(instance=Picture())
     if request.method == 'POST':
-        form = PictureForm(request.POST, request.FILES, instance=Picture())
-        if form.is_valid():
-            pic = form.save(commit=False)
-            pic.user = request.user
-            pic.save()
-            return redirect(to='app_photo:pictures')
-    return render(request, 'app_photo/upload.html', context={'form': form})
+        desc = request.POST.get('description')
+        Picture.objects.filter(pk=pic_id, user=request.user).update(description=desc)
+        return redirect(to='app_photo:pictures')
+    pic = Picture.objects.filter(pk=pic_id, user=request.user).first()
+    return render(request, 'app_photo/edit_desc.html', context={'pic': pic})
 
 
 @login_required
 def remove(request, pic_id):
-    form = PictureForm(instance=Picture())
-    if request.method == 'POST':
-        form = PictureForm(request.POST, request.FILES, instance=Picture())
-        if form.is_valid():
-            pic = form.save(commit=False)
-            pic.user = request.user
-            pic.save()
-            return redirect(to='app_photo:pictures')
-    return render(request, 'app_photo/upload.html', context={'form': form})
+    pic = Picture.objects.filter(pk=pic_id, user=request.user)
+    file_path: Path = settings.MEDIA_ROOT / str(pic.first().path)
+    if file_path.exists():
+        file_path.unlink()
+        pic.delete()
+        print('Removed file')
+    else:
+        print('File do not removed')
+
+    return redirect(to='app_photo:pictures')
